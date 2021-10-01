2520962457f236f1e992220e85a790b99b37d7b7
ErDmYLmZ <erdemyilmaz4@gmail.com>
2021-10-02 00:01:49 +0200

    3rdCommit

---
+++ src/test/java/com/techproed/Day01_FirstMavenClass.java
@@ -0,0 +1,40 @@
+package com.techproed;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Day01_FirstMavenClass {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.get("https://www.amazon.com");
+
+        driver.close();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
