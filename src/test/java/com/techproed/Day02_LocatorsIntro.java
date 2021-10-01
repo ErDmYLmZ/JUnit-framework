0087eb7f1e1557bfc71a9715b255ef3d29e65cc0
ErDmYLmZ <erdemyilmaz4@gmail.com>
2021-10-02 00:36:00 +0200

    1stCommit

--- src/test/java/com/techproed/Day02_LocatorsIntro.java
+++ src/test/java/com/techproed/Day02_LocatorsIntro.java
@@ -1,7 +1,10 @@
 package com.techproed;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.*;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
